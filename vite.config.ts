096d18fc577f80c2323b35eb0c092dd6f4c71883
ideanet <wangzebin@zupn.cn>
2021-11-04 18:01:36 +0800

    chore: 配置jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
       eslintOptions: {
         cache: false, // 禁用缓存
       },
+    }),
+    vueJsx({
+      // 配置选项
     }),
   ],
 })
