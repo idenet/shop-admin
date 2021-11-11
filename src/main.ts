428b0e51b11d56a65d938f8b00c6a117d06f35bf
idenet <wangzebin@zupn.cn>
2021-11-11 18:14:59 +0800

    chore: 更换ele和vxe的位置，不然无法打包

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
+import elementPlus from './plugins/element-plus'
 import 'xe-utils'
 import VXETable from 'vxe-table'
 import 'vxe-table/lib/style.css'
-import elementPlus from './plugins/element-plus'
 
 import './styles/index.scss'
 
@@ -14,7 +14,7 @@ const app = createApp(App)
 app.use(router)
 app.use(elementPlus)
 app.use(store, key)
-// app.use(VXETable)
+app.use(VXETable)
 // 自动注册全局组件
 const modules = import.meta.globEager('./composables/**/index.ts')
 for (const path in modules) {
