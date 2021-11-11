0c4b8565ba3e2a36841483706aed5c54cf0e93e9
idenet <wangzebin@zupn.cn>
2021-11-11 18:01:52 +0800

    feat: 解决bug

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
+import elementPlus from './plugins/element-plus'
 import 'xe-utils'
 import VXETable from 'vxe-table'
 import 'vxe-table/lib/style.css'
-import elementPlus from './plugins/element-plus'
 
 import './styles/index.scss'
 
 
 const app = createApp(App)
+
+app.use(router)
+app.use(store, key)
+app.use(elementPlus)
+app.use(VXETable)
 // 自动注册全局组件
 const modules = import.meta.globEager('./composables/**/index.ts')
 for (const path in modules) {
   app.use(modules[path].default)
 }
 
-app.use(router).use(elementPlus).use(store, key).use(VXETable).mount('#app')
+app.mount('#app')
